840cd5066d759b59f41a5e225cc235243f390318
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-08 17:44:16 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,9 @@
 class SightingsController < ApplicationController
 
   def show
+    sighting = Sighting.find_by(id: params[:id])
+    if sighting
+      render json = sightings, include: [:bird, :location]
   end
-  
+
 end
