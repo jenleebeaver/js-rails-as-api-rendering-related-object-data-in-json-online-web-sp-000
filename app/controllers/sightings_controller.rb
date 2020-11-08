2ff9b36b995fbeed9c331fae9c47e235877e042b
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-08 17:45:11 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,6 +4,10 @@ class SightingsController < ApplicationController
     sighting = Sighting.find_by(id: params[:id])
     if sighting
       render json = sightings, include: [:bird, :location]
+      #can use code below to show implied .to_json method in rails
+      #render json: sighting.to_json(include: [:bird, :location])
+    else
+      
   end
 
 end
