6a8852b5e7676724592e8ed9821718703dc29d91
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-08 17:45:33 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -7,7 +7,8 @@ class SightingsController < ApplicationController
       #can use code below to show implied .to_json method in rails
       #render json: sighting.to_json(include: [:bird, :location])
     else
-      
+      render json: {message: 'No sighting found with that id'}
+    end
   end
 
 end
